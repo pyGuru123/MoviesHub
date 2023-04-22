22ae560af945eeb6136e36a87e2df32e3d2eba90
pyguru123 <pyguru123@gmail.com>
2023-04-22 19:50:35 +0530

    added movieshub bot

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
+import asyncio
 from flask import render_template, request, json, jsonify, Response, flash, redirect, url_for, session
 
 from app import app
-from app.script import get_movie_url
+from app.script import get_movie_url, send_post
 
 
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html", shorten_url=None)
+    return render_template("index.html", shorten_url=None, message=None)
 
 @app.route("/movieurl", methods=["GET", "POST"])
 def movieurl():
@@ -17,4 +18,34 @@ def movieurl():
         url = request.form["moviehash"]
         shorten_url = get_movie_url(name, url)
 
-        return render_template("index.html", shorten_url=shorten_url)
+        return render_template("index.html", shorten_url=shorten_url, message=None)
+
+@app.route("/postbot", methods=["GET", "POST"])
+def postbot():
+    if request.method == "POST":
+        image_url = request.form.get("imageurl")
+        caption = request.form.get("caption")
+        text480 = request.form.get("text480")
+        url480 = request.form.get("url480")
+        text720 = request.form.get("text720")
+        url720 = request.form.get("url720")
+        text1080 = request.form.get("text1080")
+        url1080 = request.form.get("url1080")
+
+        buttons = []
+        if url480:
+            buttons.append([text480, url480])
+        if url720:
+            buttons.append([text720, url720])
+        if url1080:
+            buttons.append([text1080, url1080])
+
+        data = {
+            "image_url" : image_url,
+            "caption" : caption,
+            "buttons" : buttons
+        }
+
+        asyncio.run(send_post(data))
+
+    return render_template("index.html", shorten_url=None, message="Post Sent Successfully")
--- app/script.py
+++ app/script.py
@@ -1,5 +1,19 @@
+import os
 import json
+import asyncio
+import telegram
+from dotenv import load_dotenv
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from app.utils import fetch_posts, random_post, aeshash
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+PUBLIC_CHANNEL = os.getenv("PUBLIC_CHANNEL")
+PRIVATE_CHANNEL = os.getenv("PRIVATE_CHANNEL")
+BIN_CHANNEL = os.getenv("BIN_CHANNEL")
+
+bot = telegram.Bot(token=BOT_TOKEN)
 
 def get_movie_url(name, url):
     try:
@@ -16,3 +30,27 @@ def get_movie_url(name, url):
             
     except Exception as e:
         print(e)
+
+
+async def send_post(data):
+    print(data) 
+    image_url = data["image_url"]
+    caption = data["caption"]
+    buttons = data["buttons"]
+
+    row1 = []
+    row2 = []
+    for button in buttons[:2]:
+        row1.append(InlineKeyboardButton(button[0], url=button[1]))
+    if len(buttons) == 3:
+        row2.append(InlineKeyboardButton(buttons[2][0], url=buttons[2][1]))
+
+    if len(buttons) <= 2:
+        reply_markup = InlineKeyboardMarkup([row1])
+    elif len(buttons) == 3:
+        reply_markup = InlineKeyboardMarkup([row1, row2])
+# PUBLIC_CHANNEL, PRIVATE_CHANNEL, 
+    chat_ids = [BIN_CHANNEL]
+
+    for _id in chat_ids:
+        await bot.send_photo(chat_id=_id, photo=image_url, caption=caption, reply_markup=reply_markup)
